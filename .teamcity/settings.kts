125e6c8a54ff0fbdf5f124088b4346e0e5364228
ChubatovaTiger <78346593+ChubatovaTiger@users.noreply.github.com>
2022-02-15 11:03:46 +0100

    Update settings.kts

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -62,7 +62,7 @@ object Positive : BuildType({
         script {
             scriptContent = """
                 echo a
-                exit 0
+                exit 1
             """.trimIndent()
         }
     }
